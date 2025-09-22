cccee74c7a15364af593cbf639c5a2ceb0307a03
Cbtdeus <cbtdeus@proton.me>
2025-09-22 15:11:51 +0300

    tests

--- src/test/java/io/github/kitrinaludex/file_uploader/FileUploaderApplicationTests.java
+++ src/test/java/io/github/kitrinaludex/file_uploader/FileUploaderApplicationTests.java
@@ -7,9 +7,10 @@ import io.github.kitrinaludex.file_uploader.model.Folder;
 import io.github.kitrinaludex.file_uploader.repository.FileRepository;
 import io.github.kitrinaludex.file_uploader.repository.UserRepository;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Optional;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Order;
@@ -20,20 +21,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class FileUploaderApplicationTests {
 
+  public static String testFolderUuid;
   @Autowired
   UserRepository userRepository;
   @Autowired
   FileRepository fileRepository;
   @Autowired
   private TestRestTemplate restTemplate;
-  public static String testFolderUuid;
-
   @Value("${uploadDirectory}")
   private String uploadDirectory;
 
@@ -45,7 +51,6 @@ class FileUploaderApplicationTests {
 
   @Nested
   @Suite
-
   class AuthTestSuite {
     @Test
 
@@ -60,8 +65,6 @@ class FileUploaderApplicationTests {
     @Test
     public void canRegisterSuccessfully() {
 
-//          HttpHeaders headers = new HttpHeaders();
-//          headers.setBasicAuth("demo","demo");
       SignupRequest signupRequest = new SignupRequest("zxcqwe123", "asdfzx");
       assertThat(restTemplate
           .postForEntity("/register", signupRequest, String.class).getStatusCode()).isEqualTo(
@@ -71,20 +74,18 @@ class FileUploaderApplicationTests {
     }
 
   }
+
   @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
   @Nested
   @Suite
   class FolderTestSuite {
 
-
     @Test
     @Order(1)
-    //folders?name=coolfolder3&parentUuid=c5a5f8ee-146c-4858-a1dc-828a1a34517e
     public void canCreateFolders() {
       ResponseEntity<?> response = restTemplate.withBasicAuth("demo", "demo")
           .postForEntity("/folders?name=coolfolder", "", String.class);
       testFolderUuid = (String) response.getBody();
-      System.out.println(testFolderUuid);
       assertThat(response.getStatusCode())
           .isEqualTo(HttpStatusCode.valueOf(200)); //should be 201
       assertThat(fileRepository.findFolderByUuid(testFolderUuid)).isPresent();
@@ -96,9 +97,9 @@ class FileUploaderApplicationTests {
     @Test
     @Order(2)
     public void canCreateNestedFolders() {
-      System.out.println("nested uuid:" + testFolderUuid);
       ResponseEntity<?> response = restTemplate.withBasicAuth("demo", "demo")
-          .postForEntity("/folders?name=coolfolder2&parentUuid=" + testFolderUuid, "", String.class);
+          .postForEntity("/folders?name=coolfolder2&parentUuid=" + testFolderUuid, "",
+              String.class);
       assertThat(response.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(200)); //should be 201
       Optional<Folder> createdFolder = fileRepository.findFolderByUuid((String) response.getBody());
 
@@ -115,7 +116,7 @@ class FileUploaderApplicationTests {
 
       try {
         restTemplate.withBasicAuth("demo", "demo")
-            .put("/folders/"+ testFolderUuid + "?name=asdf", "");
+            .put("/folders/" + testFolderUuid + "?name=asdf", "");
       } catch (Exception e) {
         throw new RuntimeException(e);
       }
@@ -126,28 +127,88 @@ class FileUploaderApplicationTests {
     @Test
     @Order(4)
     void canDeleteFolders() {
+      String folderUuid;
+
+      ResponseEntity<?> response = restTemplate.withBasicAuth("demo", "demo")
+          .postForEntity("/folders?name=testfolder", "", String.class);
+      folderUuid = (String) response.getBody();
+
       try {
-        restTemplate.withBasicAuth("demo", "demo").delete("/folders/" + testFolderUuid);
-      }catch (Exception e) {
+        restTemplate.withBasicAuth("demo", "demo").delete("/folders/" + folderUuid);
+      } catch (Exception e) {
         throw new RuntimeException(e);
-      }finally {
-        assertThat(fileRepository.findFolderByUuid(testFolderUuid)).isNotPresent();
+      } finally {
+        assertThat(fileRepository.findFolderByUuid(folderUuid)).isNotPresent();
       }
-
-
     }
 
+    @Test
+    @Order(5)
+    void canDeleteFoldersWithFiles() {
+      try {
+        restTemplate.withBasicAuth("demo", "demo").delete("/folders/" + testFolderUuid);
+      } catch (Exception e) {
+        throw new RuntimeException(e);
+      } finally {
+        assertThat(fileRepository.findFolderByUuid(testFolderUuid)).isNotPresent();
+      }
+    }
   }
 
   @Nested
   @Suite
   class FileTestSuite {
 
-      void canUploadFiles() {
-
+    @Test
+    void canUploadFiles() throws IOException {
+      File tempFile = Files.createTempFile("testfile", ".bin").toFile();
+      try (FileOutputStream fos = new FileOutputStream(tempFile)) {
+        byte[] buffer = new byte[1024]; //1kb
+        for (int i = 0; i < 999; i++) {
+          fos.write(buffer);
+        }
 
       }
-  }
+      HttpHeaders headers = new HttpHeaders();
+      headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+      headers.setBasicAuth("demo", "demo");
 
+      MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+      FileSystemResource fileResource = new FileSystemResource(tempFile);
+      body.add("file", fileResource);
+      HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
+
+      ResponseEntity<String> response =
+          restTemplate.postForEntity("/files", requestEntity, String.class);
+      assertThat(response.getStatusCode().is2xxSuccessful()).isTrue();
+      tempFile.deleteOnExit();
+    }
+
+    @Test
+    void cantUploadFilesOverLimit() throws IOException {
+      File tempFile = Files.createTempFile("testfile2", ".bin").toFile();
+      try (FileOutputStream fos = new FileOutputStream(tempFile)) {
+        byte[] buffer = new byte[1024]; //1kb
+        for (int i = 0; i < 11000; i++) { // 10000 * 1kb = 10mb
+          fos.write(buffer);
+        }
+
+      }
+      HttpHeaders headers = new HttpHeaders();
+      headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+      headers.setBasicAuth("demo", "demo");
+
+      MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+      FileSystemResource fileResource = new FileSystemResource(tempFile);
+      body.add("file", fileResource);
+      HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
+
+      ResponseEntity<String> response =
+          restTemplate.postForEntity("/files", requestEntity, String.class);
+      assertThat(response.getStatusCode().is4xxClientError()).isTrue();
+      tempFile.deleteOnExit();
+    }
+
+  }
 }
 
